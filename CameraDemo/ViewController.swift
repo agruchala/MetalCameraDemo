9255777f241d4cc8feb7034d9f2d858f91e2d047
Artur Gruchała <gruchala.a@gmail.com>
2021-07-31 17:20:42 +0200

    Initial Commit

---
+++ CameraDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraDemo
+//
+//  Created by Artur Gruchała on 31/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
